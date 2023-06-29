826a8ca082c9ca562bf0b29b82f693189ad3bf5d
romneycf <romneycf@gmail.com>
2023-06-29 17:26:18 -0300

    working flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,209 +1,136 @@
 import "./App.css";
-import Box from "./components/box/box";
 import Title from "./components/title/title";
 import Labelbtn from "./components/labelbtn/labelbtn";
 import { useEffect, useRef, useState } from "react";
 
 function App() {
-  const [session, setSession] = useState({
-    lenght: 25,
-    break: 5,
-    minutes: 25,
-    seconds: 0,
-    label: "Session",
-    isRunning: false,
-  });
+  const [sessionTime, setSessionTime] = useState(25);
+  const [breakTime, setBreakTime] = useState(5);
+  const [displayTime, setDisplayTime] = useState(1500);
+  const [isActive, setActive] = useState(false);
+  const [label, setLabel] = useState("Session");
+  const audioElement = useRef(null);
 
-  const trueMinutes =
-    session.minutes < 10 ? "0" + session.minutes : session.minutes;
-  const trueSeconds =
-    session.seconds < 10 ? "0" + session.seconds : session.seconds;
+  function maskTime(time: number) {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return `${minutes < 10 ? "0" + minutes : minutes}:${
+      seconds < 10 ? "0" + seconds : seconds
+    }`;
+  }
 
-  function handleLenght(operator: "decrement" | "increment") {
-    if (operator === "increment") {
-      if (session.lenght >= 60) return false;
-      setSession((values) => ({
-        ...values,
-        lenght: session.lenght++,
-        minutes: session.minutes++,
-      }));
+  function changeTime(amount: number, type: string) {
+    let newCount;
+    if (type === "Session") {
+      newCount = sessionTime + amount;
+    } else {
+      newCount = breakTime + amount;
     }
 
-    if (operator === "decrement") {
-      if (session.lenght <= 1) return false;
-      setSession((values) => ({
-        ...values,
-        lenght: session.lenght--,
-        minutes: session.minutes--,
-      }));
+    if (newCount > 0 && newCount <= 60 && !isActive) {
+      type === "Session" ? setSessionTime(newCount) : setBreakTime(newCount);
+      if (type === "Session") {
+        setDisplayTime(newCount * 60);
+      }
     }
   }
 
-  function handleBreak(operator: "decrement" | "increment") {
-    if (operator === "increment") {
-      if (session.break >= 60) return false;
-      setSession((values) => ({
-        ...values,
-        break: session.break + 1,
-      }));
-    }
-
-    if (operator === "decrement") {
-      if (session.break <= 1) return false;
-      setSession((values) => ({
-        ...values,
-        break: session.break - 1,
-      }));
-    }
+  function start() {
+    setActive(!isActive);
   }
 
-  function pause() {
-    setSession((values) => ({
-      ...values,
-      isRunning: false,
-    }));
-  }
+  useEffect(() => {
+    if (isActive && displayTime > 0) {
+      const interval = setInterval(() => {
+        setDisplayTime(displayTime - 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    } else if (displayTime === 0) {
+      console.log("acabou");
+      //audioElement.current.play();
+      //audioElement.current.currentTime = 0;
 
-  function resume() {
-    setSession((values) => ({
-      ...values,
-      isRunning: true,
-    }));
+      //    setTimeout(() => {
+      if (label === "Session") {
+        setDisplayTime(breakTime * 60);
+        setLabel("Break");
+      }
+      if (label === "Break") {
+        setDisplayTime(sessionTime * 60);
+        setLabel("Session");
+      }
+    }
+  }, [breakTime, sessionTime, displayTime, label, isActive]);
+
+  function resetTime() {
+    setBreakTime(5);
+    setSessionTime(25);
+    setDisplayTime(1500);
+    setLabel("Session");
+    setActive(false);
+    clearInterval(undefined);
+    //audioElement.current.load();
   }
 
   return (
     <div className="App">
-      <Box>
-        <Title>25 plus 5 Clock</Title>
+      <div className="display">
         <div className="display-center">
-          <div className="controller-wrapper">
+          <div className="wrapper break-wrapper">
             <label id="break-label">Break Length</label>
             <div>
               <Labelbtn
                 id="break-decrement"
-                onClick={() => handleBreak("decrement")}
+                onClick={() => changeTime(-1, "Break")}
               >
                 <i className="fa fa-arrow-down"></i>
               </Labelbtn>
-              <label id="break-length">{session.break}</label>
+              <label id="break-length">{`${breakTime}`}</label>
               <Labelbtn
                 id="break-increment"
-                onClick={() => handleBreak("increment")}
+                onClick={() => changeTime(1, "Break")}
               >
                 <i className="fa fa-arrow-up"></i>
               </Labelbtn>
             </div>
           </div>
-          <div className="controller-wrapper">
-            <p>{session.isRunning ? "Running" : "Not running"}</p>
-            <label id="timer-label">{session.label}</label>
-            <span id="time-left">
-              {trueMinutes}:{trueSeconds}
-            </span>
+          <div className="wrapper session-wrapper">
+            <label id="timer-label">{label}</label>
+            <span id="time-left">{maskTime(displayTime)}</span>
             <div>
-              <Labelbtn
-                id="start_stop"
-                onClick={session.isRunning ? pause : resume}
-              >
+              <Labelbtn id="start_stop" onClick={start}>
                 <i
-                  className={
-                    !session.isRunning ? "fa fa-forward-step" : "fa fa-pause"
-                  }
+                  className={!isActive ? "fa fa-forward-step" : "fa fa-pause"}
                 ></i>
               </Labelbtn>
-              <Labelbtn id="session-decrement">
+              <Labelbtn id="reset" onClick={resetTime}>
                 <i className="fa fa-refresh"></i>
               </Labelbtn>
             </div>
           </div>
-          <div className="controller-wrapper">
+          <div className="wrapper length-wrapper">
             <label id="session-label">Session Length</label>
             <div>
               <Labelbtn
                 id="session-decrement"
-                onClick={() => handleLenght("decrement")}
+                onClick={() => changeTime(-1, "Session")}
               >
                 <i className="fa fa-arrow-down"></i>
               </Labelbtn>
-              <label id="session-length">{trueMinutes}</label>
+              <label id="session-length">{sessionTime}</label>
               <Labelbtn
                 id="session-increment"
-                onClick={() => handleLenght("increment")}
+                onClick={() => changeTime(1, "Session")}
               >
                 <i className="fa fa-arrow-up"></i>
               </Labelbtn>
             </div>
           </div>
         </div>
-      </Box>
+        <span className="title">25 plus 5 Clock - by R. Freire</span>
+      </div>
     </div>
   );
 }
 
 export default App;
-/*
-import { useTimer } from 'react-timer-hook';
-import React, { useState, useEffect } from 'react';
-import Modal from '../Modal/Modal';
-import Button from '../Button/Button';
-import { theme } from '../../styles/theme';
-import RenovacaoJwt from './ModalJwt';
-
-const { rbmWhite, dangerColor } = theme.colors;
-
-const Timer = ({ expiryTimestamp }) => {
-  const [logout, setLogout] = useState(false);
-  const [renovation, setRenovation] = useState(false);
-
-
-   const { seconds, minutes, hours } = useTimer({
-    expiryTimestamp,
-    onExpire: () => {
-      setRenovation(false);
-      setLogout(true);
-    },
-  });
-
-  const trueSeconds = seconds < 10 ? '0' + seconds : seconds;
-  const trueMinutes = minutes < 10 ? '0' + minutes : minutes;
-  const trueHours = hours < 10 ? '0' + hours : hours;
- 
-
-  useEffect(() => {
-   hours == 0 && minutes == 0 && seconds == 59 && setRenovation(true);
-  }, [minutes]);
-
-  return (
-    <div style={{ textAlign: 'center',width:'100%' }}>
-      <span>{trueHours + ":" + trueMinutes + ':' + trueSeconds}</span>
-      <RenovacaoJwt open={renovation} close={() => setRenovation(false)} />
-      <Modal  open={logout} close={true} >
-        <>
-          <div style={{ padding: '1rem' }}>
-            <h1>{'SUA SESSÃO FOI EXPIRADA'}</h1>
-          </div>
-          <div style={{ padding: '1rem' }}>
-            <Button
-              backgroundColor={dangerColor}
-              width={'13.792vw'}
-              height={'3.125vw'}
-              hoverBGColor={dangerColor}
-              hoverColor={rbmWhite}
-              color={rbmWhite}
-              onClick={() => {
-                window.location = '/login';
-                localStorage.clear();
-              }}
-              roleName={'button-confirmar-negar'}
-            >
-              <span>OK</span>
-            </Button>
-          </div>
-        </>
-      </Modal>
-    </div>
-  );
-};
-
-export default Timer;
-*/
--- src/components/box/box.tsx
+++
@@ -1,6 +0,0 @@
-import React, { ReactNode } from "react";
-import styles from "./box.module.css";
-
-export default function Box( { children }: { children: ReactNode}) {
-    return <div className={styles.container}>{children}</div>
-}
